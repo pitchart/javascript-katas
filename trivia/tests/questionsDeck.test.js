67418ff3691cde4978cfcf31a7f09d2fc23f5bd6
Etienne Giraudet <etienne.giraudet@decathlon.com>
2023-01-04 12:07:19 +0100

    feat : add next question function

--- trivia/tests/questionsDeck.test.js
+++ trivia/tests/questionsDeck.test.js
@@ -21,6 +21,13 @@ class QuestionsDeck {
     count(category) {
         return this.questions.get(category).length;
     }
+
+    getNextQuestion(category) {
+        if(!this.questions.has(category) || this.questions.get(category).length <=0) {
+            throw new Error("no more question");
+        }
+        return this.questions.get(category).shift();
+    }
 };
 
 describe("questions deck", () => {
@@ -45,5 +52,33 @@ describe("questions deck", () => {
 
         expect(addSecondQuestion).toThrow("Question already exists");
         expect(addQuestionToOtherCategory).toThrow("Question already exists");
+    });
+
+    it("should retrieve the next question for a category", () => {
+        const deck = new QuestionsDeck();
+
+        deck.addQuestion("category-name", "question-name");
+        deck.addQuestion("category-name", "question-name2");
+        deck.addQuestion("category-name2","question-name3");
+        deck.addQuestion("category-name2","question-name4");
+
+        let nextQuestion = deck.getNextQuestion("category-name");
+        expect(nextQuestion).toBe("question-name");
+        nextQuestion = deck.getNextQuestion("category-name");
+        expect(nextQuestion).toBe("question-name2");
+    });
+
+    it("should not have no more questions", () => {
+        const deck = new QuestionsDeck();
+        deck.addQuestion("category-name", "question-name");
+        deck.getNextQuestion("category-name");
+        const nextQuestion = () =>  deck.getNextQuestion("category-name");
+        expect(nextQuestion).toThrow("no more question");
+    });
+
+    it("should not ask question on not existing category", () => {
+        const deck = new QuestionsDeck();
+        const nextQuestion = () =>  deck.getNextQuestion("category-name");
+        expect(nextQuestion).toThrow("no more question");
     });
 });

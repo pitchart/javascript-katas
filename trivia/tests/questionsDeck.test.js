c8fa63c1f49863e5e31f9200378345ed52d3615b
Axel LEBAS <axel.lebas@decathlon.com>
2023-01-04 11:31:44 +0100

    feat: avoid adding same questions

--- trivia/tests/questionsDeck.test.js
+++ trivia/tests/questionsDeck.test.js
@@ -3,7 +3,14 @@ class QuestionsDeck {
     questions = new Map();
 
     addQuestion(category, label) {
-        this.questions.set(category, []);
+        if (!this.questions.has(category)) {
+            this.questions.set(category, []);
+        }
+
+        if (this.questions.get(category).includes(label)) {
+            throw new Error("Question already exists");
+        }
+
         this.questions.get(category).push(label);
     }
 
@@ -13,13 +20,26 @@ class QuestionsDeck {
 };
 
 describe("questions deck", () => {
-
-    it("should have questions for a category", () => {
+    it("should have questions for categories", () => {
         const deck = new QuestionsDeck();
 
         deck.addQuestion("category-name", "question-name");
+        deck.addQuestion("category-name", "question-name2");
 
-        expect(deck.count("category-name")).toBe(1);
+        deck.addQuestion("category-name2", "question-name3");
+
+        expect(deck.count("category-name")).toBe(2);
+        expect(deck.count("category-name2")).toBe(1);
     });
-    
+
+    it("should not have two same questions for a category", () => {
+        const deck = new QuestionsDeck();
+
+        deck.addQuestion("category-name", "question-name");
+        const addSecondQuestion = () => deck.addQuestion("category-name", "question-name");
+        const addQuestionToOtherCategory = () => deck.addQuestion("category-name2", "question-name");
+
+        expect(addSecondQuestion).toThrow("Question already exists");
+        expect(addQuestionToOtherCategory).toThrow("Question already exists");
+    });
 });
